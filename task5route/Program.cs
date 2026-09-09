de8dd68faf4e44b9898c73d378a3d570e7a2cd85
mohamedyasserhossny <my226546@gmail.com>
2026-09-09 05:06:36 -0700

    question 1

--- task5route/Program.cs
+++ task5route/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question 1
+             book b1 = new book();
+            b1.password;
+            // can not decleared here because private can declere in it,s scoop only can not decleared out side scoop or out project 
+            #endregion
         }
     }
 }
